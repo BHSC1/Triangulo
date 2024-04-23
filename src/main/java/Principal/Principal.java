3173ee3d362c84c6a8c76eabdbbf9b6c2b0db851
1072419878 <1072419878@UNS11DT098927.animaedu.intranet>
2024-04-23 20:36:22 -0300

    Cal triangulo

---
+++ src/main/java/Principal/Principal.java
@@ -0,0 +1,11 @@
+package Principal;
+
+import Visao.FrmTriangulo;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        FrmTriangulo janela = new FrmTriangulo();
+        janela.setVisible(true);
+    }
+}
